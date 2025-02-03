a3bcd0c8ce8b3e7d63184ad363d27147c913f2db
magnus <magnusmichaelson@protonmail.com>
2025-02-03 15:54:44 +0000

    :)

--- api.js
+++ api.js
@@ -1,10 +1,43 @@
-// express.js handles all the api stuff
+
 const express = require('./node_modules/express');
 const app = express();
 
-app.put('/', (req, res) => {
-  console.log(req);
-  return res.send('Received a PUT HTTP method');
+const changeRange = (
+  servoSettingText,
+) => {
+  let response = 'not understood';
+  const servoSetting = parseInt(servoSettingText, 10);
+  if (!Number.isNaN(servoSetting)) {
+    response = `range set to ${servoSetting}`;
+    console.log(`servo set to ${servoSetting}`);
+  }
+  return response;
+};
+
+app.get('/', (req, res) => {
+  let response = 'not understood';
+
+  // check for servo update
+  if (Object.prototype.hasOwnProperty.call(req.query, 'range0001')) {
+    response = changeRange(req.query['range0001']);
+  }
+
+  // check for servo update
+  if (Object.prototype.hasOwnProperty.call(req.query, 'textInput0001')) {
+    response = changeRange(req.query['textInput0001']);
+  }
+
+  // check for servo update
+  if (Object.prototype.hasOwnProperty.call(req.query, 'button0001')) {
+    response = changeRange(req.query['button0001']);
+  }
+
+  // check for servo update
+  if (Object.prototype.hasOwnProperty.call(req.query, 'checkbox0001')) {
+    response = changeRange(req.query['checkbox0001']);
+  }
+  console.log()
+  return res.json({'no': 'worries'});
 });
 
 // start the api
@@ -12,8 +45,6 @@ app.listen(8000, () => {
   console.clear();
   console.log();
   console.log('api listening on port 8000');
-  console.log()
-  console.log('in a browser, go to the raspberry pi ip address')
   console.log();
 });
 
