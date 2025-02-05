8b39c7b439ec5ae5d5ebb38dd8e83563485adf51
magnus <magnusmichaelson@protonmail.com>
2025-02-05 21:28:59 +0000

    :)

--- api.js
+++ api.js
@@ -1,20 +1,15 @@
-// Requiring module
 const express = require('express')
 const cors = require('cors')
 const app = express()
-
-
 app.use(cors());
 
 app.get('/', (req, res) => {
   console.log(req.query);
-  //
+  
   let response = {'mission': 'failed'};
-  //
+  
   if (Object.prototype.hasOwnProperty.call(req.query, 'buttonPressed')) {
-    //
     response = {'mission': 'accomplished'};
-    //
   }
   res.json(response);
 });
@@ -22,9 +17,7 @@ app.get('/', (req, res) => {
 // start the api
 app.listen(8000, () => {
   console.clear();
-  console.log();
   console.log('api listening on port 8000');
-  console.log();
 });
 
 
