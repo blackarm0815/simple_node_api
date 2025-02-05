6fe6659771a35925f05991f3018eba065e5db999
magnus <magnusmichaelson@protonmail.com>
2025-02-05 18:21:16 +0000

    :)

--- api.js
+++ api.js
@@ -6,56 +6,13 @@ const app = express()
 
 app.use(cors());
 
-const changeRange = (
-  rangeValueText,
-) => {
-  let response = 'not understood';
-  const rangeValue = parseInt(rangeValueText, 10);
-  if (!Number.isNaN(rangeValue)) {
-    response = `range set to ${rangeValue}`;
-  }
-  return response;
-};
-
-const changeText = (
-  inputText,
-) => {
-  return `text input = ${inputText}`;
-};
-
-const changeButton = () => {
-  return 'button0001 was pressed';
-};
-
-const changeCheckbox = (
-  inputText,
-) => {
-  return `checkbox0001 = ${inputText}`;
-};
-
 app.get('/', (req, res) => {
-  let response = 'not understood';
-
-  // check for servo update
-  if (Object.prototype.hasOwnProperty.call(req.query, 'range0001')) {
-    response = changeRange(req.query['range0001']);
-  }
-
-  // check for servo update
-  if (Object.prototype.hasOwnProperty.call(req.query, 'textInput0001')) {
-    response = changeText(req.query['textInput0001']);
-  }
-
-  // check for servo update
+  let response = 'not recognized';
+  console.log(req.query);
   if (Object.prototype.hasOwnProperty.call(req.query, 'button0001')) {
-    response = changeButton();
+    response = `result is ${req.query['button0001']}`;
   }
-
-  // check for servo update
-  if (Object.prototype.hasOwnProperty.call(req.query, 'checkbox0001')) {
-    response = changeCheckbox(req.query['checkbox0001']);
-  }
-  return res.send().text('testing123');
+  return res.send().text(response);
 });
 
 // start the api
